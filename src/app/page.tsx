39080fbd81407311366e7fda04fd51c39e00d593
menfem <connor.royes@me.com>
2025-05-12 11:06:20 +0100

    fixed linting and type issues

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from "react"
 import Link from "next/link"
-import { Circle } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import { cn } from "@/lib/utils"
